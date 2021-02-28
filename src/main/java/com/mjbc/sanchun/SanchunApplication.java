dba6d9211cc2a3d3299e37d0f35208f1ba287c4d
khwan <khwan@khwanui-MacBook-Pro.local>
2021-02-28 20:44:29 +0900

    Initial commit

---
+++ src/main/java/com/mjbc/sanchun/SanchunApplication.java
@@ -0,0 +1,13 @@
+package com.mjbc.sanchun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanchunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanchunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjbc/sanchun/SanchunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjbc.sanchun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanchunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
